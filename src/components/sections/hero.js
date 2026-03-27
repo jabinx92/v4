d861d67593710d7306cb5260c93e86d4cbe9e160
jabinx92 <jabinx92@gmail.com>
2026-03-27 10:22:35 -0700

    change front page text

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,29 +59,20 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Jonathan Won.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>Hello World,</h1>;
+  const two = <h2 className="big-heading">I am Jonathan Won.</h2>;
+  const three = <h3 className="big-heading">I build things for the web. 👾🤖</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I'm a software engineer based in San Francisco, CA specializing in building websites and
+        applications with beautiful typefaces.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="jabinx92@gmail.com" target="_blank" rel="noreferrer">
+      Get In Touch
     </a>
   );
 
