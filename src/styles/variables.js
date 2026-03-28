ec36a1566d7d54c9cf7d18720ad975d34e87deb4
jabinx92 <jabinx92@gmail.com>
2026-03-27 19:34:18 -0700

    change styling part 2

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,9 +2,9 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
+    --dark-navy: #020204;
+    --navy: #020204;
+    --light-navy: rgb(24, 24, 24);
     --lightest-navy: #233554;
     --navy-shadow: rgba(2, 12, 27, 0.7);
     --dark-slate: #495670;
@@ -12,7 +12,7 @@ const variables = css`
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #64ffda;
+    --green: #22b455;
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
