9727ff380b2f1e22ab7e5e7319d9611c6b18b4ad
jabinx92 <jabinx92@gmail.com>
2026-07-30 12:45:18 -0700

    Prevent stale portfolio caching

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,11 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+
+export const onClientEntry = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .getRegistrations()
+      .then(registrations => registrations.forEach(registration => registration.unregister()));
+  }
+};
